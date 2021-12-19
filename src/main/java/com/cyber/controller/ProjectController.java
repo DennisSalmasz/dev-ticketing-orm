b1030f1224ffa684c68585f4798d4fd1d3858aa1
DennisSalmaz <deniz.salmazs@gmail.com>
2021-12-19 23:23:45 +0000

    project mapping - update & delete

--- src/main/java/com/cyber/controller/ProjectController.java
+++ src/main/java/com/cyber/controller/ProjectController.java
@@ -1,21 +1,14 @@
 package com.cyber.controller;
 
 import com.cyber.dto.ProjectDTO;
-import com.cyber.dto.TaskDTO;
-import com.cyber.dto.UserDTO;
-import com.cyber.enums.Status;
 import com.cyber.service.ProjectService;
 import com.cyber.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/project")
@@ -29,16 +22,11 @@ public class ProjectController {
         this.userService = userService;
     }
 
-//    @Autowired
-//    TaskService taskService;
-
     @GetMapping("/create")
     public String createProject(Model model){
-
         model.addAttribute("project", new ProjectDTO());
         model.addAttribute("managers",userService.listAllByRole("manager")); // assign managers
         model.addAttribute("projects",projectService.listAllProjects());
-
         return "/project/create";
     }
 
@@ -73,36 +61,4 @@ public class ProjectController {
         projectService.update(project);
         return "redirect:/project/create";
     }
-
-//    @GetMapping("/manager/complete")
-//    public String getProjectsByManager(Model model){
-//
-//        //I wanna see project belong to John
-//        UserDTO manager = userService.findById("john@ticketng.com");
-//
-//        List<ProjectDTO> projects = getCountedListOfProjectDTO(manager);
-//        model.addAttribute("projects",projects);
-//
-//        return "manager/project-status";
-//    }
-//
-//    List<ProjectDTO> getCountedListOfProjectDTO(UserDTO manager){
-//
-//        List<ProjectDTO> list = projectService
-//                .findAll()
-//                .stream()
-//                .filter(p -> p.getAssignedManager().equals(manager))
-//                .map(p -> {
-//                    List<TaskDTO> taskList = taskService.findTaskManager(manager);
-//                    int completeCount = (int) taskList.stream().filter( t -> t.getProject().equals(p) && t.getTaskStatus() == Status.COMPLETE).count();
-//                    int incompleteCount = (int) taskList.stream().filter( t -> t.getProject().equals(p) && t.getTaskStatus() != Status.COMPLETE).count();
-//
-//                    p.setCompleteTaskCount(completeCount);
-//                    p.setIncompleteTaskCount(incompleteCount);
-//
-//                    return p;
-//
-//        }).collect(Collectors.toList());
-//        return list;
-//    }
 }
